39ffc8b71027bc4f3f930f1f34408f21c3ffdfdd
EXB04891 <agustina.aliciardi@darwoft.com>
2023-08-29 09:43:58 -0300

    Se agregaron tests de Yvytu

---
+++ cypress/e2e/3-yvytu/yvytu.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe("Test sobre la página de YVYTU", () => {
+  beforeEach(() => {
+    cy.visit("https://vientosdelaselva.com.ar/");
+  });
+  it("Verificar Texto Principal", () => {
+    const subtitle =
+      "\n              Conocé la selva paranaense desde el corazón \n              del área de mayor biodiversidad de Argentina.\n            ";
+    cy.get("h1").should("have.text", "Reserva Yvytu");
+    cy.get("h2").first().should("have.text", subtitle);
+  });
+});
